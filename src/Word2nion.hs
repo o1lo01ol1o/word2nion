cae7647b7c17f1cfc173cd918df8f37431ce5cb9
o1lo01ol1o <tim.pierson@gmail.com>
2020-06-29 19:43:26 +0100

    add deps

--- src/Word2nion.hs
+++ src/Word2nion.hs
@@ -1,15 +1,8 @@
-{- |
-Copyright: (c) 2020 o1lo01ol1o
-SPDX-License-Identifier: NONE
-Maintainer: o1lo01ol1o <tim.pierson@gmail.com>
-
-See README for more info
--}
-
-module Word2nion
+module HasktorchSkeleton
        ( someFunc
        ) where
 
+import Torch.Typed.NN ()
 
 someFunc :: IO ()
 someFunc = putStrLn ("someFunc" :: String)
