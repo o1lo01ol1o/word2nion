1e74b54677c29ff9f655db1e19b635dddeacaba5
o1lo01ol1o <tim.pierson@gmail.com>
2020-07-02 17:22:08 +0100

    finish initialization

--- src/Torch/Typed/Quaternion.hs
+++ src/Torch/Typed/Quaternion.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ConstraintKinds         #-}
 {-# LANGUAGE DataKinds               #-}
 {-# LANGUAGE DeriveGeneric           #-}
+{-# LANGUAGE DerivingStrategies      #-}
 {-# LANGUAGE FlexibleContexts        #-}
 {-# LANGUAGE FlexibleInstances       #-}
 {-# LANGUAGE GADTs                   #-}
@@ -24,13 +25,29 @@
 
 -- | Module defining operations on quaternions.
 -- Ported from https://github.com/Orkis-Research/Pytorch-Quaternion-Neural-Networks/blob/master/core_qnn/quaternion_ops.py
-module Torch.Typed.Quaternion ((⦿), hamilton, HasQuaternion, NQuaternions, DivisionProofs) where
+module Torch.Typed.Quaternion
+  ( (⦿),
+    hamilton,
+    initialize,
+    normalize,
+    Quaternions (..),
+    catQuaternions,
+    HasQuaternion,
+    NQuaternions,
+    DivisionProofs,
+  )
+where
 
+import           GHC.Generics          (Generic)
 import           GHC.TypeLits
-import           GHC.TypeNats         ()
-import           Prelude              hiding (sqrt, sum)
-import qualified Torch.Internal.Class as ATen
-import qualified Torch.Tensor         as D
+import           GHC.TypeNats          ()
+import           Prelude               hiding (cos, pi, sin, sqrt, sum)
+import qualified Prelude               as P (sqrt)
+import           Torch.Initializers    (FanMode (..), NonLinearity (..),
+                                        kaimingUniform)
+import qualified Torch.Internal.Class  as ATen
+import qualified Torch.Tensor          as D
+import           Torch.TensorFactories (randIO')
 import           Torch.Typed
 
 type family NQuaternions (features :: Nat) where
@@ -47,7 +64,11 @@ type DivisionProofs n d =
       ~ ((n - Div n d) + Div n d)
   )
 
-r, i, j, k ::
+-- | Accessors
+r,
+  i,
+  j,
+  k ::
     forall batchSize featureSize device dtype.
     (DivisionProofs featureSize 4, All KnownNat '[batchSize, featureSize]) =>
     Tensor device dtype '[batchSize, featureSize] ->
@@ -58,6 +79,7 @@ j = narrow @1 @(featureSize `Div` 2) @(featureSize `Div` 4)
 k = narrow @1 @(featureSize - (featureSize `Div` 4)) @(featureSize `Div` 4)
 
 data Modulous = ModulousScalar | ModulousVector
+  deriving stock (Show, Eq, Ord, Enum, Bounded, Generic)
 
 data SingModulous m where
   SModulousScalar :: SingModulous 'ModulousScalar
@@ -108,6 +130,9 @@ normalize t' = t' / (addScalar eps modu)
         . reshape @'[batchSize, 1]
         $ modulous SModulousScalar t'
 
+-- | Helper that specializes the cat function for the shapes
+-- needed below.  Helps type inference work out that `hamilton`
+-- is well typed.
 qcat ::
   forall
     batchSize
@@ -168,3 +193,49 @@ infix 5 ⦿
   Tensor device dtype '[batchSize, featureSize] ->
   Tensor device dtype '[batchSize, featureSize]
 a ⦿ b = hamilton a b
+
+data InitializationScheme = InitializationSchemeGlorot | InitializationSchemeHe
+  deriving stock (Show, Eq, Ord, Enum, Bounded, Generic)
+
+data Quaternions batch features device dtype = Quaternions
+  { quaternions_r :: Tensor device dtype '[batch, features],
+    quaternions_i :: Tensor device dtype '[batch, features],
+    quaternions_j :: Tensor device dtype '[batch, features],
+    quaternions_k :: Tensor device dtype '[batch, features]
+  }
+
+catQuaternions :: Quaternions batch features device dtype -> Tensor device dtype '[batch, features * 4]
+catQuaternions (Quaternions q_r q_i q_j q_k) = cat @1 (q_r :. q_i :. q_j :. q_k :. HNil)
+
+-- | Weight initialization
+initialize ::
+  forall nIn nOut dtype device.
+  ( SumDType dtype ~ dtype,
+    KnownDevice device,
+    StandardFloatingPointDTypeValidation
+      device
+      dtype,
+    SumDTypeIsValid device dtype,
+    All KnownNat '[nIn, nOut]
+  ) =>
+  IO (Quaternions nIn nOut device dtype)
+initialize = do
+  (modulus :: Tensor device dtype '[nIn, nOut]) <- UnsafeMkTensor <$> kaimingUniform FanIn (LeakyRelu $ P.sqrt (0.0 :: Float)) [fan_out, fan_in]
+  (v :: Tensor device dtype '[nIn * nOut, 3]) <- UnsafeMkTensor <$> randUnif [fan_out * fan_in, 3]
+  (phase :: Tensor device dtype '[nIn, nOut]) <- UnsafeMkTensor . (* pi) <$> randUnif [fan_out, fan_in]
+  let vnorm = sqrt . sumDim @1 $ (addScalar eps (v ^ (2 :: Int)))
+      v' = v / (expand @'[nIn * nOut, 3] True $ reshape @'[nIn * nOut, 1] vnorm)
+      v_i = reshape @'[nIn, nOut] $ narrow @1 @0 @1 v'
+      v_j = reshape @'[nIn, nOut] $ narrow @1 @1 @1 v'
+      v_k = reshape @'[nIn, nOut] $ narrow @1 @2 @1 v'
+      weight_r = modulus * cos phase
+      weight_i = modulus * v_i * sin (phase)
+      weight_j = modulus * v_j * sin (phase)
+      weight_k = modulus * v_k * sin (phase)
+  return $ Quaternions weight_r weight_i weight_j weight_k
+  where
+    pi = 3.141592653589793238462643383279502884197169399375105820974944
+    eps = 0.0001 :: Float
+    randUnif = fmap ((\x -> x - 1) . (* 2)) . randIO'
+    fan_in = natValI @nIn
+    fan_out = natValI @nOut
