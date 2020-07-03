561e721fb44e7d42f33edc748b4a2f9a3cd2fdd6
o1lo01ol1o <tim.pierson@gmail.com>
2020-07-04 00:12:59 +0100

    happy fun effienct time

--- src/Torch/Streamly/Dataloader.hs
+++ src/Torch/Streamly/Dataloader.hs
@@ -20,55 +20,71 @@
 
 module Torch.Streamly.Dataloader
   ( TokenStreamDataset (..),
+    dataset,
     trainStream,
   )
 where
 
 import Control.Monad.Catch (MonadCatch)
-import Control.Monad.IO.Class
-  ( MonadIO,
-    liftIO,
-  )
+import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Bifunctor (bimap)
-import qualified Data.IntervalMap.Generic.Strict as IM
 import Data.IntervalMap.Generic.Strict (IntervalMap)
+import qualified Data.IntervalMap.Generic.Strict as IM
 import qualified Data.IntervalMap.Generic.Strict as IvMap
 import Data.List (sortOn)
-import qualified Data.Map.Monoidal.Strict as MMap
 import Data.Map.Monoidal.Strict (MonoidalMap)
+import qualified Data.Map.Monoidal.Strict as MMap
 import qualified Data.Maybe as Maybe (mapMaybe)
-import Data.Monoid (Sum (..))
+import Data.Monoid (First (..), Sum (..))
 import qualified Data.Set as Set
 import qualified Data.Text as T
-import Data.Text (Text)
-import Foreign.Storable ()
-import Foreign.Storable.Generic
+import Foreign.Storable
 import GHC.Generics (Generic)
 import Path
 import Streamly
 import qualified Streamly.Data.Fold as FL
 import qualified Streamly.Internal.Data.Fold as FL
-import Streamly.Internal.Data.Unicode.Stream
-  ( decodeUtf8,
-  )
+import Streamly.Internal.Data.Unicode.Stream (decodeUtf8)
 import qualified Streamly.Internal.FileSystem.File as File
 import qualified Streamly.Internal.Memory.Array as SA
-import Streamly.Prelude as S hiding (minimum, tail)
+import Streamly.Prelude as S hiding
+  ( minimum,
+    tail,
+  )
 import System.Random (randomRIO)
 import Torch.Streamly.Tokenizer
 
 -- | Represents a dataset of some token type `a`
 data TokenStreamDataset a = TokenStreamDataset
-  { -- | The buffered, tokenized corpora in a streamly Array for O(1) random access
-    tokenStreamDatasetData :: SA.Array a,
-    -- | The frequencies of the tokens in the corpora
+  { -- | The lookup map of tokens to integers
+    tokenStreamDatasetToken :: MonoidalMap a (First Int),
+    -- | The buffered, tokenized corpora in a streamly Array for O(1) random access
+    tokenStreamDatasetData :: SA.Array Int,
+    -- | The counts of the tokens in the corpora
     tokenStreamDatasetFrequencies :: MonoidalMap a (Sum Int),
     -- | The probability intervals per token (for sampling from the noise distribution)
-    tokenStreamDatasetProbabilities :: IntervalMap (StrictTuple Double Double) a,
+    tokenStreamDatasetProbabilities :: IntervalMap (StrictTuple Double Double) Int,
     -- | The total number of examples
     tokenStreamDatasetOccurances :: Int
   }
   deriving stock (Eq, Show, Ord, Generic)
+
+-- These are not so efficient, but it was convienient to be able to merge to two datasets so easily.
+--
+instance (Ord a) => Semigroup (TokenStreamDataset a) where
+  (<>) (TokenStreamDataset ta da fa _ oa) (TokenStreamDataset tb db fb _ ob) = TokenStreamDataset lkups (da <> db) f' p' o
+    where
+      lkups = (ta <> tb)
+      f' = fa <> fb
+      o = oa + ob
+      mm = MMap.mapKeys (fromJust . getFirst . (lkups MMap.!)) f'
+      p' = toIvMap o mm
+      fromJust (Just v) = v
+      fromJust _ = error "Semigroup (TokenStreamDataset a): fromJust shouldn't be partial here!  Something is wrong!"
+
+instance (Ord a) => Monoid (TokenStreamDataset a) where
+  mempty = TokenStreamDataset mempty (SA.fromList []) mempty (IM.fromList []) 0
+  mappend = (<>)
 
 data StrictTuple a b = StrictTuple !a !b
   deriving stock (Eq, Show, Ord, Generic)
@@ -87,18 +103,16 @@ tokenStream fp =
   where
     splitChars = Set.fromList $ ['.', '!', '?']
 
-newtype SymbolOrToken = SymbolOrToken {unSymbolOrToken :: Either Char Text}
+newtype SymbolOrToken = SymbolOrToken {unSymbolOrToken :: Either Char T.Text}
   deriving stock (Eq, Ord, Generic)
-  deriving newtype (GStorable)
 
-deriving instance (GStorable (Either Char Text))
+-- deriving newtype (Storable, Num, Real, Enum, Integral)
 
-filterAndLowercase :: (MonadAsync m) => AheadT m Token -> AheadT m SymbolOrToken
+filterAndLowercase :: (MonadAsync m) => AheadT m Token -> AheadT m (Either Char T.Text)
 filterAndLowercase = S.mapMaybe go
   where
-    go (Token t) = Just . SymbolOrToken . Right $ T.toLower t
-    go (Symbol t) = Just . SymbolOrToken . Left $ t
-    go (Symbol t) = Just . SymbolOrToken . Left $ t
+    go (Token t) = Just . Right $ T.toLower t
+    go (Symbol t) = Just . Left $ t
     go _ = Nothing
 
 {-# INLINE groupBy #-}
@@ -116,7 +130,7 @@ groupBy kf vf = FL.Fold step' initial' extract'
     step' kv a = return $ maybe kv (\k -> kv <> MMap.singleton k (vf a)) (kf a)
     extract' = return
 
-tokenFrequencies :: (MonadAsync m) => AheadT m SymbolOrToken -> m (MonoidalMap SymbolOrToken (Sum Int))
+tokenFrequencies :: (MonadAsync m, Ord a) => AheadT m a -> m (MonoidalMap a (Sum Int))
 tokenFrequencies = S.fold (groupBy Just (const $ Sum 1)) . serially . aheadly
 
 byTwos :: [b] -> [(b, b)]
@@ -130,13 +144,22 @@ toIvMap total mm =
     go ((Just a, lb), (_, ub)) = Just $ ((StrictTuple lb ub), a)
     go _ = Nothing
 
+-- | Make a dataset from a file.  This implementaiton is a bit annoying because whatever we put in the
+-- Array needs to have a Storeable instance.  This means that we can't do unbounded containers
+-- and rolling our own datatypes requres writing the storeable instance.  So we move everything to Int maps
+-- and store the array of Ints.
 dataset :: (MonadAsync m) => Path a File -> m (TokenStreamDataset SymbolOrToken)
 dataset fp = do
-  prepped <- liftIO . SA.fromStream . serially . aheadly . filterAndLowercase $ tokenStream fp
-  let stream = SA.toStream prepped
-  mm <- tokenFrequencies $ stream
-  let total = getSum . mconcat $ MMap.elems mm
-  pure $ TokenStreamDataset prepped mm (toIvMap total mm) total
+  prepped <- liftIO . S.toList . serially . aheadly . filterAndLowercase $ tokenStream fp
+  mm' <- tokenFrequencies $ S.fromList prepped
+  let total = getSum . mconcat $ MMap.elems mm'
+  let lkups = MMap.fromList $ zip (MMap.keys mm') (First . Just <$> [0 ..])
+  let mm = MMap.mapKeys (fromJust . getFirst . (lkups MMap.!)) mm'
+  prepped' <- SA.fromStream . serially . aheadly . S.map (fromJust . getFirst . (lkups MMap.!)) $ S.fromList prepped
+  pure $ TokenStreamDataset (MMap.mapKeys SymbolOrToken lkups) prepped' (MMap.mapKeys SymbolOrToken mm') (toIvMap total mm) total
+  where
+    fromJust (Just v) = v
+    fromJust _ = error "dataset: fromJust shouldn't be partial here!  Something is wrong!"
 
 -- | Subsample infrequent words occurding to a probability derived from the words frequency
 -- in the corpora. The original paper used the same caclulation as below on
@@ -176,7 +199,9 @@ sample size a = S.concatMapM (const return ()) $ do
   start <- liftIO $ randomRIO (0, (SA.length a - size))
   pure . S.fromList $ fmap (SA.unsafeIndex a) [start .. (start + size)]
 
-trainStream :: (MonadAsync m, Storable a, Ord a) => Int -> TokenStreamDataset a -> (AheadT m [a], AheadT m a)
-trainStream i (TokenStreamDataset a mm im c) = (trainStream', negativeSampleStream im)
+trainStream :: (MonadAsync m, Ord a) => Int -> TokenStreamDataset a -> (AheadT m [Int], AheadT m Int)
+trainStream i (TokenStreamDataset lkups a mm im c) = (trainStream', negativeSampleStream im)
   where
-    trainStream' = S.mapMaybeM (subsampleFilter c mm) $ shufflingStreamOfSize i a
+    trainStream' = S.mapMaybeM (subsampleFilter c (MMap.mapKeys (fromJust . getFirst . (lkups MMap.!)) mm)) $ shufflingStreamOfSize i a
+    fromJust (Just v) = v
+    fromJust _ = error "trainStream: fromJust shouldn't be partial here!  Something is wrong!"
